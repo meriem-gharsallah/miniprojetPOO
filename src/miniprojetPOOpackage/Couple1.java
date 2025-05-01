fcd7e9736f340e359766d220a64a9d72d6abd299
meriem-gharsallah <meriem.gharsallah@etudiant-enit.utm.tn>
2025-05-01 14:52:22 +0100

    modification1

---
+++ src/miniprojetPOOpackage/Couple1.java
@@ -0,0 +1,13 @@
+package miniprojetPOOpackage;
+
+public class Couple1 {
+	public Nom nom1;
+    public double s;
+   
+
+    public Couple1(Nom nom1, double s) {
+        this.nom1 = nom1;
+        this.s = s;
+      
+    }
+}
