4228400d87eded3cc03fe14f60cb151022da9cba
meriem-gharsallah <meriem.gharsallah@etudiant-enit.utm.tn>
2025-05-09 22:09:22 +0100

    ajout de normalisation

--- src/miniprojetPOOpackage/MainComparateurNomDecompose.java
+++ src/miniprojetPOOpackage/MainComparateurNomDecompose.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MainComparateurNomDecompose {
     public static void main(String[] args) {
         Nom n1 = new Nom("NK-24isa6tapAYCqtEnqbUdzv", "DEMIDOVICH     VASILIJ ");
-        Nom n2 = new Nom("NK-24isa6tapAYCqtEnqbUdzv", "DEMIDOVICH     VASILIJ fff");
+        Nom n2 = new Nom("NK-24isa6tapAYCqtEnqbUdzv", "DEMIDOVICH     VASILIJ jj hhh");
         List<String> listeDesNomsComparateursDeChaines=new ArrayList<String>();
         listeDesNomsComparateursDeChaines.add("Levenshtein");
         listeDesNomsComparateursDeChaines.add("JaroWinkler");
--- src/miniprojetPOOpackage/Normalisation.java
+++ src/miniprojetPOOpackage/Normalisation.java
@@ -5,13 +5,33 @@ import java.util.List;
 
 public class Normalisation implements Pretraiteur {
 
-	public List<Nom> pretraiter(List<Nom> noms) {
-		List<Nom> L2 = new ArrayList<>();
-		return L2;
-	}
-	public String toString() {
+    @Override
+    public List<Nom> pretraiter(List<Nom> noms) {
+        List<Nom> resultat = new ArrayList<>();
+
+        for (Nom nom : noms) {
+        	// 1. On transforme les caractères accentus en combinaison lettre + accent
+            String decompose = java.text.Normalizer.normalize(nom.getNom(), java.text.Normalizer.Form.NFD);
+            // 2. On enlève les signes d'accent (diacritiques)
+        	String texteNormalise = decompose.replaceAll("[\\p{M}]", "")
+        									 .toLowerCase()
+                                             .trim()
+                                             .replaceAll("\\s+", " ") // remplacer plusieurs espaces par un seul
+        	                                 .replaceAll("\\d", "")
+        									 .replaceAll("[^\\p{L}\\p{Z}]", "");
+        	/*p{L}	Toutes les lettres Unicode
+        	\\p{M}	Les marques diacritiques (accents combinés)
+        	\\p{Z}	Les espaces Unicode (y compris  tab)*/
+        	
+            Nom normalise = new Nom(nom.getId(), texteNormalise);
+            resultat.add(normalise);
+        }
+
+        return resultat;
+    }
+
+    @Override
+    public String toString() {
         return "Normalisation";
     }
 }
-
-
